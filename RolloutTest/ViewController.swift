9d4605fc0f02f22fd17a3996f7d16cedfc2bd4a4
Marin <marintutucv@gmail.com>
2021-03-22 18:53:12 +0200

    Initial Commit

---
+++ RolloutTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RolloutTest
+//
+//  Created by Marin Tutuc on 22.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
